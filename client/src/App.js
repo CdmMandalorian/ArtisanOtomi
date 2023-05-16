01086eeba07e1465d18bee89fe2d5f6eee34a430
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2023-05-16 10:48:17 -0700

    set useEffect, useState const to fetch /api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
 import React, { useEffect, useState} from 'react'
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <div>
 
